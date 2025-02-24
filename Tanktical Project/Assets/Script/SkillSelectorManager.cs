cb7304c70c5d0431a9cf031244e83c88b7be6771
lNoshirol <145337387+lNoshirol@users.noreply.github.com>
2025-02-24 23:11:24 +0100

    new tag ally + ennemy, SkillSelectionManager

--- Tanktical Project/Assets/Script/SkillSelectorManager.cs
+++ Tanktical Project/Assets/Script/SkillSelectorManager.cs
@@ -6,6 +6,8 @@ public class SkillSelectorManager : MonoBehaviour
 {
     public static SkillSelectorManager Instance;
 
+    public SkillsSandBox.Skill LastSkillSelected {  get; private set; }
+
     private void Awake()
     {
         if (Instance == null)
@@ -16,5 +18,15 @@ public class SkillSelectorManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Update()
+    {
+        LastSkillSelected.SkillSelected();
+    }
+
+    public void SetSelectedSkill(SkillsSandBox.Skill skill)
+    {
+        LastSkillSelected = skill;
     }
 }
